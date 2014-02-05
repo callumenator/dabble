2b4ffd336144b0248fe85aebf237816dafdbfbba
Callum Anderson <callumenator@gmail.com>
2014-02-05 16:34:47 +1100

    Another update test

--- dabble/repl.d
+++ dabble/repl.d
@@ -696,7 +696,7 @@ bool testCompile(out DMDMessage[] errors)
 
     auto rawFile = File(srcFile, "w");
     rawFile.write(context.rawCode.toString());
-    rawFile.close();   
+    rawFile.close(); 
      
     if (errFile.exists())
         try { errFile.remove(); } catch(Exception e) {}
